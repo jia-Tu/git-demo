0d87303cded441c434406078720b0ab794befad1
jia-Tu <1347555496@qq.com>
2022-07-03 11:13:40 +0800

    first commit

--- src/main/java/git/jiaTu/GitTest.java
+++ src/main/java/git/jiaTu/GitTest.java
@@ -3,5 +3,6 @@ package git.jiaTu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git~");
+        System.out.println("hello git~");
     }
 }
