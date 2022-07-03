819dd610059b4e12526e736c57233da49dd6db1b
jia-Tu <1347555496@qq.com>
2022-07-03 13:13:51 +0800

    hot-fix fourth commit

--- src/main/java/git/jiaTu/GitTest.java
+++ src/main/java/git/jiaTu/GitTest.java
@@ -6,6 +6,5 @@ public class GitTest {
         System.out.println("hello git~");
         System.out.println("hello git~");
         System.out.println("hello hot-fix~");
-        System.out.println("hello hot-fix~");
     }
 }
