13c22f36e34728fb6b7e797429d2e2300549d9ab
jia-Tu <108558126+jia-Tu@users.noreply.github.com>
2022-07-03 15:08:18 +0800

    pull

--- src/main/java/git/jiaTu/GitTest.java
+++ src/main/java/git/jiaTu/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello hot-master1~");
         System.out.println("hello hot-fix1~");
         System.out.println("hello hot-fix1~");
+         System.out.println("hello pull~");
     }
 }
